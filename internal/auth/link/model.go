a65e762d73e2a0aa9fb59b1949ebe5b674221730
charon <1charon1@example.com>
2026-04-06 16:16:02 +0300

    del env

---
+++ internal/auth/link/model.go
@@ -0,0 +1,30 @@
+package link
+
+import (
+	"math/rand"
+
+	"gorm.io/gorm"
+)
+
+type Link struct {
+	gorm.Model
+	Url  string `json:"url"`
+	Hash string `json:"hash" gorm:"uniqueIndex"`
+}
+
+func NewLink(url string) *Link {
+	return &Link{
+		Url:  url,
+		Hash: RandStringRunes(10),
+	}
+}
+
+var letterRunes = []rune("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890")
+
+func RandStringRunes(n int) string {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(res)
+}
