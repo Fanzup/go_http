91eb5118380ac14875ae600744a427dad580e49d
charon <1charon1@example.com>
2026-04-06 18:32:58 +0300

    link

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"demo/weather_check/configs"
 	"demo/weather_check/internal/auth"
+	"demo/weather_check/internal/auth/link"
 	"demo/weather_check/packages/db"
 	"fmt"
 	"net/http"
@@ -12,9 +13,13 @@ func main() {
 	conf := configs.LoadConfig()
 	_ = db.NewDb(conf)
 	router := http.NewServeMux()
+
+	//Handler
 	auth.NewHelloHandler(router, auth.AuthHandlerDeps{
 		Config: conf,
 	})
+
+	link.NewLinkHandler(router, link.LinkHandlerDeps{})
 
 	server := http.Server{
 		Addr:    ":8081",
